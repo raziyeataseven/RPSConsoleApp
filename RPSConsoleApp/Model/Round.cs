78be3224dae5175d48e5c6508f1b00d999757c07
Raziye Ataseven <raziye.ataseven@dk.global-bilgi.com.tr>
2022-02-19 13:57:59 +0300

    Add project files.

---
+++ RPSConsoleApp/Model/Round.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSConsoleApp.Model
+{
+    public class Round
+    {
+        public Round()
+        {
+            var rnd = new Random();
+            int move = rnd.Next(1, 4);
+            Move = (Move)move;
+
+            switch (Move)
+            {
+                case Move.Paper:
+                    Status = Status.Win;
+                    break;
+                case Move.Rock:
+                    Status = Status.Scoreless;
+                    break;
+                case Move.Scissor:
+                    Status = Status.Lose;
+                    break;
+                default:
+                    break;
+            }
+        }
+        public readonly Move Move;
+        public readonly Status Status;
+    }
+
+    public enum Move
+    {
+        Rock = 1,
+        Paper = 2,
+        Scissor = 3
+    }
+
+    public enum Status
+    {
+        Win,
+        Lose,
+        Scoreless
+    }
+}
---
+++ RPSConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using RPSConsoleApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RPSConsoleApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Shall we start to play rock, paper, scissors? (Y/N)");
+            string command = Console.ReadLine();
+            if (command.ToUpper().StartsWith("Y"))
+            {
+                Console.WriteLine("Player 1   |   Player 2");
+                Console.WriteLine("-----------------------");
+
+                var player = new Player();
+                player.Rounds = new List<Round>();
+
+                for (var i = 0; i < 100; i++)
+                {
+                    var round = new Round();                   
+                    player.Rounds.Add(round);
+                    Console.WriteLine("Rock       |   {0} ({1})", round.Move.ToString(), round.Status.ToString());
+                }
+                Console.WriteLine("");
+                Console.WriteLine("~~~~Game Summary~~~~");
+                Console.WriteLine("");
+                Console.WriteLine("Player 1   Wins: {0} Loses: {1} ScoreLess: {2}", player.Rounds.Count(f=>f.Status == Status.Lose), player.Rounds.Count(f=>f.Status == Status.Win), player.Rounds.Count(f=>f.Status == Status.Scoreless));
+                Console.WriteLine("");
+                Console.WriteLine("Player 2   Wins: {0} Loses: {1} ScoreLess: {2}", player.Rounds.Count(f => f.Status == Status.Win), player.Rounds.Count(f => f.Status == Status.Lose), player.Rounds.Count(f => f.Status == Status.Scoreless));
+
+            }
+            else
+                Console.WriteLine("Maybe next time! :)");
+
+        }
+    }
+}
